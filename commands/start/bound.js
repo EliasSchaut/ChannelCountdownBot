36c8e68fa7983b0c289e1d682c8b191aefa4541e
Elias Lorenz Schaut <uvmvb@student.kit.edu>
2021-05-12 21:37:04 +0200

    build up

---
+++ commands/start/bound.js
@@ -0,0 +1,14 @@
+module.exports = {
+    name: 'bound',
+    description: "Bound Voice Channel to Timer",
+    aliases: ['link', 'b'],
+    args: true,
+    args_min_length: 1,
+    usage: "[channel id]",
+    guildOnly: true,
+    dmOnly: false,
+    restricted: true,
+    execute(message, args) {
+
+    },
+};
