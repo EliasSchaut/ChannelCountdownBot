ff0cded6ea0256d8da9cf80b35155c0b0a4ba162
Elias Schaut <elias.schaut@student.kit.edu>
2021-05-27 23:49:38 +0200

    make bound useable from bot side

--- commands/start/bound.js
+++ commands/start/bound.js
@@ -1,4 +1,4 @@
-const config = require("../../config/config.json");
+const timer = require("../../js/timer.js")
 
 module.exports = {
     name: 'bound',
@@ -6,69 +6,22 @@ module.exports = {
     aliases: ['link', 'b'],
     args: true,
     args_min_length: 1,
-    usage: "[time index]",
+    usage: "[channel_id]|[time_format]",
     guildOnly: true,
     dmOnly: false,
     restricted: true,
     execute(message, args) {
-        const channel = message.guild.channels.cache.get(config.bday_channel);
-        const log = message.guild.channels.cache.get(config.log_channel);
+        args = args.join(" ").split("|")
+        if (args.length !== 2) {
+            return message.reply("Wrong args. You should use this format: [channel_id],[time_format]");
+        }
+
+        const channel = message.guild.channels.cache.get(args[0].trim());
         if (channel === undefined) {
             return message.reply("Can't find the given channel id");
         }
 
-        let now = new Date();
-        let bday = new Date(config.time[args[0]]);
-
-        let diff;
-        let days;
-        let hours;
-        let mins;
-        let out;
-        const prefix_out = "Join in "
-
-        function loop() {
-            now = new Date();
-            bday = new Date(config.time[args[0]]);
-
-            diff = (bday.getTime() - now.getTime()) / 1000;
-            diff /= 60;
-            diff = Math.round(diff);
-            console.log("diff: " + diff)
-
-            if (diff <= 0) {
-                console.log("stop")
-                channel.setName("JOIN NOW");
-                channel.updateOverwrite(channel.guild.roles.everyone, {CONNECT: true});
-                clearInterval(timerID);
-                return message.reply("Voice channel is now free");
-            }
-
-            days = Math.floor(diff / (60 * 24));
-            hours = Math.floor((diff - (days * 60 * 24)) / 60);
-            mins = diff % 60
-
-            out = format(days, hours, mins)
-            channel.setName(prefix_out + out);
-            log.send("format: " + out);
-        }
-        const timerID = setInterval(loop, 5 * 60 * 1000);
-        console.log("start")
-        message.reply("Bot started! End at: " + config.time[args[0]]);
-
-        function format(days, hours, mins) {
-            let out = "";
-            if (days !== 0) {
-                out += days + "d ";
-            }
-            if (hours !== 0) {
-                out += hours + "h ";
-            }
-            if (mins !== 0) {
-                out += mins + "min ";
-            }
-
-            return out;
-        }
+        channel.updateOverwrite(channel.guild.roles.everyone, {CONNECT: false});
+        timer.timer(message, channel, args[1].trim())
     },
 };
---
+++ js/timer.js
@@ -0,0 +1,83 @@
+const config = require('../config/config.json')
+
+function timer(message, channel, time_format) {
+
+    // -----------------------------------
+    // Needed Variables
+    // -----------------------------------
+    const log = message.guild.channels.cache.get(config.log_channel);
+    let now = new Date();
+    let bday = new Date(time_format);
+    if (isNaN(bday.getTime())) return message.reply("your time format is wrong!")
+
+    let diff;
+    let days;
+    let hours;
+    let mins;
+    let out;
+    const prefix_out = "Join in "
+    // -----------------------------------
+
+
+    // -----------------------------------
+    // Rename Looper
+    // -----------------------------------
+    function loop() {
+        now = new Date();
+        bday = new Date(time_format)
+        diff = (bday.getTime() - now.getTime()) / 1000;
+        diff /= 60;
+        diff = Math.round(diff);
+        console.log("diff: " + diff)
+
+        if (diff <= 0) {
+            console.log("stop")
+            channel.setName("JOIN NOW");
+            channel.updateOverwrite(channel.guild.roles.everyone, {CONNECT: true});
+            clearInterval(timerID);
+            return message.reply("Voice channel is now free");
+        }
+
+        days = Math.floor(diff / (60 * 24));
+        hours = Math.floor((diff - (days * 60 * 24)) / 60);
+        mins = diff % 60
+
+        out = format(days, hours, mins)
+        channel.setName(prefix_out + out);
+        if (config.log) {
+            log.send("format: " + out);
+        }
+    }
+    // -----------------------------------
+
+
+    // -----------------------------------
+    // Start Loop
+    // -----------------------------------
+    const timerID = setInterval(loop, 5 * 60 * 1000);loop();
+    console.log("start")
+    message.reply("Bot started! End at: " + time_format);
+    // -----------------------------------
+
+
+    // -----------------------------------
+    // Helper
+    // -----------------------------------
+    function format(days, hours, mins) {
+        let out = "";
+        if (days !== 0) {
+            out += days + "d ";
+        }
+        if (hours !== 0) {
+            out += hours + "h ";
+        }
+        if (mins !== 0) {
+            out += mins + "min ";
+        }
+
+        return out;
+    }
+    // -----------------------------------
+}
+
+module.exports = {timer}
